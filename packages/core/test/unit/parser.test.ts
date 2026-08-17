724bd55b70fc505ef5ec4e9a9039a5f799d08c68
Dmytro Leonov <leonovdmitriy02@gmail.com>
2026-08-17 07:56:51 +0300

    chore: run biome

--- packages/core/test/unit/parser.test.ts
+++ packages/core/test/unit/parser.test.ts
@@ -12,6 +12,6 @@ describe(createChevrotainParser, () => {
       ' (asdf1..& ..123 &asdf..asdf2 & ..asdf3 |kw:1| kw:null) & ( kw:!( !asdf & asdf ) ) & asdf !=null ',
     );
     const res = parser.parse(tokens);
-    expect(res.errors).toStrictEqual([])
+    expect(res.errors).toStrictEqual([]);
   });
 });
